6ca1de62fa370f8574f54c1d6d969af71669e0f8
JuanDavid24 <david.juan90.jd@gmail.com>
2023-11-17 18:40:23 -0300

    mission 4: node + servidor express estático

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require ('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+app.listen(4000, () => console.log("Servidor corriendo en http://localhost:4000"));
