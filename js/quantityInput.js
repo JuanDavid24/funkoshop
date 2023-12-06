d58d3aeb673a378132215fc23b717a8ccb414e1c
JuanDavid24 <david.juan90.jd@gmail.com>
2023-12-06 16:52:37 -0300

    [FEAT] (M3) quantity-btn [FIX] slider en item.html

---
+++ js/quantityInput.js
@@ -0,0 +1,25 @@
+let inputList = document.querySelectorAll(".quantity-input");
+
+inputList.forEach(input => input.addEventListener( "change", () => {
+    if (input.value < 0) {
+        alert("Error: La cantidad mínima es 0")
+        input.value = 0;
+    }
+}));
+
+let buttonList = document.querySelectorAll(".quantity-btn");
+
+buttonList.forEach(btn => btn.addEventListener("click", () => {
+    let input = btn.parentElement.parentElement.querySelector(".quantity-input");
+    changeInputValue(btn, input);
+}));
+
+const changeInputValue = (btn, input) => {
+    let btnClasses = Array.from(btn.classList);  
+    if (btnClasses.includes("quantity-btn--add"))
+        input.value++;
+    if (btnClasses.includes("quantity-btn--substract")) {
+        if (input.value != 0) 
+            input.value--
+    } 
+}
