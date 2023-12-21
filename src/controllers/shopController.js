2dd232383021d0dfcdb992e932e811616ec43150
JuanDavid24 <david.juan90.jd@gmail.com>
2023-12-20 23:35:47 -0300

    [FEAT] M5: cart muestra datos dinámicos

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -11,7 +11,6 @@ const shopControllers = {
     })},
 
     itemView: (req, res) => {
-        const url = req.url;
         const itemId = req.params.id;
         const item = data.find( element => element.product_id == itemId );
         const relatedItems = data.filter( element => element.licence_name == item.licence_name );
@@ -25,10 +24,14 @@ const shopControllers = {
     },
 
     addItem: (req, res) => res.send('addItem route'),
-    cartView: (req, res) => res.render(path.join(__dirname, '../views/shop/cart.ejs'), {
-        title: "Carrito",
-        isAdmin
-    }),
+    cartView: (req, res) => {
+        
+        res.render(path.join(__dirname, '../views/shop/cart.ejs'), {
+            title: "Carrito",
+            isAdmin,
+            data
+        })
+    },
 
     addToCart: (req, res) => res.send('addToCart route')
 }
