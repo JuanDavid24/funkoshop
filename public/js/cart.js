9bf89a3bd1399a7ddc613fc68347bf79d73739eb
JuanDavid24 <david.juan90.jd@gmail.com>
2024-01-21 22:59:39 -0300

    console.log borrados

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,15 +1,10 @@
 let inputs = document.querySelectorAll(".quantity-input");
 
-inputs.forEach(input => input.addEventListener( "change", () => {
-  const totalPriceDOM = input.parentElement.parentElement.querySelector(".product__total-price");
-  const unitPriceDOM = input.parentElement.parentElement.querySelector(".product__price > .currency");
-  const unitPrice = parseFloat(unitPriceDOM.textContent.replace(/[^\d.-]/g, ''))
-
-  console.log(`precio uniterio ${unitPrice}`);
-
-  console.log(`precio convertido ${convertCurrency(unitPrice * input.value)}`);
-  totalPriceDOM.innerHTML =  convertCurrency(unitPrice * input.value)
-  console.log(`precio total actualizado de producto ${parseFloat(totalPriceDOM.innerHTML)}`);
-
-  console.log(`precio total actualizado de producto ${parseFloat(totalPriceDOM.innerHTML)}`);
-}));
+const calcularPrecios = inputs => {
+  inputs.forEach(input => input.addEventListener( "change", () => {
+    const totalPriceDOM = input.parentElement.parentElement.querySelector(".product__total-price");
+    const unitPriceDOM = input.parentElement.parentElement.querySelector(".product__price > .currency");
+    const unitPrice = parseFloat(unitPriceDOM.textContent.replace(/[^\d.-]/g, ''))
+    totalPriceDOM.innerHTML =  convertCurrency(unitPrice * input.value)
+  }));
+}
