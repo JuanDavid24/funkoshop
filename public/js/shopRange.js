65c7890d6ca568303b412d7665096a213d9a0ba7
JuanDavid24 <david.juan90.jd@gmail.com>
2024-04-25 01:22:48 -0300

    FIX: funciona rango min max en shop

--- public/js/shopRange.js
+++ public/js/shopRange.js
@@ -1,12 +1,12 @@
-import { shopFilters, arrangedItems } from "./shop.js";
-
-const minInput = document.querySelector('.filter__min');
-const maxInput = document.querySelector('.filter__max');
+import { shopFilters, updateShopFilters, arrangedItems, filterByRange } from "./shop.js";
+export const minInput = document.querySelector('.filter__min');
+export const maxInput = document.querySelector('.filter__max');
 
 // ------------------------------ Manejo inputs ------------------------------
-const setValoresIniciales = (min, max) => {
+export const initializeRangeFields = (min, max) => {
     min.value = shopFilters.min || 0;
     max.value = shopFilters.max || getPrecioMax ();
+    updateShopFilters({ 'min': Number(min.value), 'max': Number(max.value) })
 }
 
 const validarInput= ( input, fnValidacion, defaultValue=0 ) => {
@@ -19,23 +19,26 @@ const validarInput= ( input, fnValidacion, defaultValue=0 ) => {
 // ------------------------------Event listeners------------------------------
 minInput.addEventListener('change', (event) => {
    validarInput(event.target, validacionMin);
-   shopFilters.min = event.target.value;
-   sessionStorage.setItem('shopFilters', JSON.stringify(shopFilters));
+   if (shopFilters.min != event.target.value) {
+        updateShopFilters({ 'min': Number(event.target.value) });
+        filterByRange()
+   }
 });
 
 maxInput.addEventListener('change', (event) => {
     validarInput(event.target, validacionMax, getPrecioMax());
-    shopFilters.max = event.target.value;
-    sessionStorage.setItem('shopFilters', JSON.stringify(shopFilters));
+    if (shopFilters.max != event.target.value) {
+        updateShopFilters({ 'max': Number(event.target.value) });
+        filterByRange()
+   }
 });
 
-setValoresIniciales(minInput, maxInput);
-
 // ------------------------------ Aux ------------------------------
+// precio mas alto entre los productos de la tienda
 const getPrecioMax = () => {
     let maxPrice = 0;
     for (let item of arrangedItems)
-        if (maxPrice < item.price) maxPrice = item.price;
+    if (maxPrice < item.price) maxPrice = item.price;
     return Math.ceil(maxPrice)
 }
 
@@ -43,3 +46,6 @@ const getTipoInput = input => input.classList[0].slice(-3);
 
 const validacionMin = min => min >= 0 && min <= parseInt(maxInput.value); 
 const validacionMax = max => max > 0 && max >= parseInt(minInput.value);
+// ------------------------------ Filter ------------------------------
+export const filterItemsByRange = (list, min, max) => list.filter(item => min < item.price && item.price < max);
+//--------------------------------------------------------------------
